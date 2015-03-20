3314027216ce071b4bb41d1d39cacc7f608ef656
ansuz <ansuz@transitiontech.ca>
2015-03-19 22:22:33 -0400

    test.js : first test

---
+++ test.js
@@ -0,0 +1,28 @@
+var agml=require("./agml");
+
+var text=function(){/*
+
+key : value
+
+key2 : value
+
+this is a comment
+
+; this is a comment too
+
+: this is also a comment
+
+: thisIsNotAKey : thisIsNotAValue
+
+  this is valid : this is valid too ; this is a comment
+
+;   pew pew pew
+
+
+
+*/}.toString().slice(14,-3);
+
+
+var results=agml.parse(text);
+
+console.log(results);
