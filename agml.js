d59c0c238ca139b73b888c8952facb0fe51889cf
ansuz <ansuz@transitiontech.ca>
2015-04-10 21:55:32 -0400

    agml.js

--- agml.js
+++ agml.js
@@ -1,4 +1,6 @@
-var agml={};
+var agml={
+  results:[]  
+};
 
 /*
   agml parse takes three arguments
@@ -16,39 +18,75 @@ agml.parse=function(text,results,opt){
   // objects are passed by reference in javascript
   // so you can pass an object like a pointer in C
   // and agml.parse will load the results into that
-  results=results||{};
+
+  results=results||agml.results; 
   opt=opt||{};
-  opt.delim=opt.delim||':';
-  opt.trail=opt.trail||';';
+  var delim=opt.delim||':';
+  var trail=opt.trail||';';
+  var block=opt.block||'-';
+  var replace=opt.replace||true;
 
-  var leadingComment=new RegExp('^\\s*'+opt.delim);
-  var keyPattern=new RegExp('.*?'+opt.delim);
-  var trailingComment=new RegExp(opt.trail+'.*$');
+  var leadingComment=new RegExp('^\\s*'+delim);
+  var keyPattern=new RegExp('.*?'+delim);
+  var trailingComment=new RegExp(trail+'.*$');
+  var agmlBlock= new RegExp('\\'+block+'{3}(.|\s|\n)+?\\'+block+'{3}','mg');
 
-  // split into individual lines
-  // agml can't split key-value pairs across lines
-  text.split('\n')
-    // throw away empty lines, or lines with whitespace as keys
-    .filter(function(line){
-      return (!line || leadingComment.test(line))?false:true;
-    })
-    // parse each line
-    .forEach(function(line){
-      // a line is only relevant if it contains a colon
-      if(/:/.test(line)){
-        var key,val;
-        // extract the key, assign the remainder to the value
-        val=line.replace(keyPattern,function(k){
-          // but don't keep the colon
-          key=k.slice(0,-1);
-          return '';
-          // also, throw away anything after a semicolon
-        }).replace(trailingComment,'');
-        // add your results to a dictionary
-        results[key.trim()]=val.trim();
+  var toJSON=function(AGML){
+    var temp={};
+
+    // split into individual lines
+    // agml can't split key-value pairs across lines
+    AGML.split('\n')
+      // throw away empty lines, or lines with whitespace as keys
+      .filter(function(line){
+        return (!line || leadingComment.test(line))?false:true;
+      })
+      // parse each line
+      .forEach(function(line){
+        // a line is only relevant if it contains a colon
+        if(/:/.test(line)){
+          var key,val;
+          // extract the key, assign the remainder to the value
+          val=line.replace(keyPattern,function(k){
+            // but don't keep the colon
+            key=k.slice(0,-1);
+            return '';
+            // also, throw away anything after a semicolon
+          }).replace(trailingComment,'');
+          // add your results to a dictionary
+          temp[key.trim()]=val.trim();
+        }
+      });
+    results.push(temp);
+  };
+
+  // if you detect at least one delimited block of AGML
+  // just parse that, and (optionally) remove it from the text
+  if(agmlBlock.test(text)){
+    (function(){
+      // a temporary array
+      var temp=[];
+
+      // parse out the content of the blocks
+      var tempText=text.replace(agmlBlock,function(block){
+        temp.push(block.slice(3,-3));  
+      });
+
+      // call 'toJSON' on each block
+      // this pushes to your results array
+      temp.forEach(toJSON);
+
+      // unless explicitly instructed not to
+      // erase the parsed blocks from the source text
+      if(replace){
+        text=tempText;
       }
-    });
-  return results;
+    }());
+  }else{
+    // just extract the values
+    // don't do anything to the source text
+    toJSON(text);
+  }
 };
 
 /*
@@ -61,32 +99,11 @@ agml.parse=function(text,results,opt){
 
 agml.encode=function(dict,opt){
   opt=opt||{};
-  opt.delim=opt.delim||':';
+  opt.delim=opt.delim||':'; 
   return Object.keys(dict)
     .map(function(key){
       return key+opt.delim+dict[key];
     }).join('\n');
-};
-
-/*
-  agml.head takes a string, a results object, and an options object
-    and returns an array
-*/
-
-agml.head=function(text,results,opt){
-  /*
-      parse a section of text
-      filter out bits contained in '---' blocks
-      extract the data, return the text
-  */
-  opt.delim=opt.delim||'-';
-
-  var results=[];
-  return text.replace(new RegExp('\\'+opt.delim+'{3}(.|\s)*?\\'+opt.delim+'{3}','mg'),function(head){
-    results.push(head.slice(3,-3));
-    return '';
-  });
-  return results;
 };
 
 module.exports=agml;
